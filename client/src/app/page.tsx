6f94d5c9c3ca371fabb2116d515285bf96e30114
aquaman122 <swj981128@gmail.com>
2024-06-18 21:41:00 +0900

    add: clone파일 추가

---
+++ client/src/app/page.tsx
@@ -0,0 +1,26 @@
+import Results from "@/component/Results";
+
+const API_KEY = process.env.API_KEY
+
+export default async function Home({ searchParams }: { searchParams:any}) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(`https://api.themoviedb.org/3/${
+    genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+  }?api_key=${API_KEY}&language=ko-KR&page=1`, 
+  { next: {revalidate: 10000 } }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+
+  const results = data.results;
+
+  return (
+    <>
+      <div>
+        <Results results={results} />
+      </div>
+    </>
+  )
+}
